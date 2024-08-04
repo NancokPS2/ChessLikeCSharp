dffc7529613a93096fc7a3b259cb5d1b60a94257
NancokPS2 <nancok@tuta.io>
2024-08-04 17:59:25 -0300

    Improved MobDisplay organization.

---
+++ Godot/Display/MobDisplay.MobComponents.cs
@@ -0,0 +1,89 @@
+using ChessLike.Entity;
+using Godot.Display.Interface;
+
+namespace Godot.Display;
+
+
+/// <summary>
+/// Add Mobs with AddMob() to make them appear in the scene.
+/// </summary>
+
+public partial class MobDisplay : Godot.Node3D
+{
+    Dictionary<Mob, MobComponents> mob_components = new();
+
+    public MobComponents AddComponents(Mob mob)
+    {
+        //Components
+        MobComponents components = new();
+        mob_components[mob] = components;
+
+        //Set component's values
+        components.SetMesh( (Mesh)MeshPreset.MOB.Duplicate() );
+        components.SetNameTag(mob.Identity.displayed_name);
+
+        return components;
+    }
+
+    public void RemoveComponents(Mob mob)
+    {
+        mob_components.Remove(mob);
+    }
+
+    public MobComponents GetComponents(Mob mob)
+    {
+        return mob_components[mob];
+    }
+
+    public void UpdateComponentPositions()
+    {
+        foreach (Mob mob in mobs)
+        {
+            MobComponents components = mob_components[mob];
+            components.mesh_instance.Position = mob.Position.ToGVector3();
+            components.name_tag.Position = mob.Position.ToGVector3() + Vector3.Up;
+        }
+    }
+
+    
+    public class MobComponents
+    {
+        public MeshInstance3D mesh_instance = new();
+        public Label3D name_tag = new();
+
+        public void SetMesh(Mesh mesh)
+        {
+            this.mesh_instance.Mesh = mesh;
+        }
+        
+        public void SetNameTag(string name)
+        {
+            this.name_tag.Text = name;
+        }
+
+        public void AddToDisplay(MobDisplay mob_display)
+        {
+            mob_display.AddChild(name_tag);
+            mob_display.AddChild(mesh_instance);
+        }
+
+    }
+
+    public class MeshPreset
+    {
+        public static readonly Mesh CELL = new BoxMesh()
+        { 
+            Size = new Godot.Vector3(1,1,1)
+        };
+
+        public static readonly Mesh MOB = new SphereMesh()
+        { 
+            Radius = 0.5f,
+            Height = 1.0f
+        };
+
+        public static readonly Mesh DEFAULT = new PrismMesh()
+        { 
+        };
+    }
+}
--- Godot/Display/MobDisplay.cs
+++ Godot/Display/MobDisplay.cs
@@ -9,32 +9,26 @@ namespace Godot.Display;
 [GlobalClass]
 public partial class MobDisplay : Godot.Node3D
 {
-    List<Mob> mobs = new();
+    UniqueList<Mob> mobs = new();
 
     public override void _Ready()
     {
         base._Ready();
         AddChild(mob_ui);
-        SetUIAnchors(0,0.7f, 0.4f, 1);
+        SetUIAnchors(0, 0.7f, 0.4f, 1);
     }
 
     public void AddMob(Mob mob)
     {
-        if (!HasMob(mob)){
-            mobs.Add(mob);
+        //Return if it already exists.
+        if(!mobs.Add(mob))
+        {
+            GD.PushError("This mob was already added.");
+            return;
         }
 
-        MeshInstance3D mesh_instance = new MeshInstance3D();
-        Label3D label = new Label3D() {Billboard = BaseMaterial3D.BillboardModeEnum.Enabled};
-
-        mesh_pool[mob] = mesh_instance;
-        name_tag_pool[mob] = label;
-
-        AddChild(mesh_instance);
-        AddChild(label);
-
-        SetMobMesh(mob, (Mesh)MeshPreset.MOB.Duplicate());
-        SetMobNameTag(mob);
+        MobComponents components = AddComponents(mob);
+        components.AddToDisplay(this);
     }
 
     public override void _Process(double delta)
@@ -42,16 +36,16 @@ public partial class MobDisplay : Godot.Node3D
         base._Process(delta);
         if (mobs.Count > 0)
         {
-            UpdateNameTagPositions();
-            UpdateMeshPositions();
+            UpdateComponentPositions();
             UpdateUI();
         }
     }
 
     public void RemoveMob(Mob mob)
     {
+        RemoveComponents(mob);
         mobs.Remove(mob);
-        mesh_pool.Remove(mob);
+        
     }
 
     public List<Mob> GetMobs()
@@ -62,30 +56,6 @@ public partial class MobDisplay : Godot.Node3D
     public bool HasMob(Mob mob)
     {
         return mobs.Contains(mob);
-    }
-
-    public void EnsureMobValid(Mob mob)
-    {
-        bool failed = false;
-        if (!HasMob(mob))
-        {
-            GD.PushError("Tried to work on a Mob that has not been added.");
-            failed = true;
-        }
-        if (!HasMeshInstance(mob))
-        {
-            GD.PushError("No mesh had been set.");
-            failed = true;
-        }
-        if (!HasNameTag(mob))
-        {
-            GD.PushError("No mesh had been set.");
-            failed = true;
-        }
-        if (failed)
-        {
-            AddMob(mob);
-        }
     }
 
 }
